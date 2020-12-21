3ac2c5b5c11fbb5c1aae0779e0d4a69e1d71185b
at-trinhnguyen2 <trinh.nguyen2@asiantech.vn>
2020-12-22 03:50:51 +0700

    fix review
    Took 27 minutes

--- src/main/java/com/ctr/homestaybooking/service/ReviewService.kt
+++ src/main/java/com/ctr/homestaybooking/service/ReviewService.kt
@@ -7,8 +7,17 @@ import com.ctr.homestaybooking.entity.ReviewEntity
 import com.ctr.homestaybooking.repository.BookingRepository
 import com.ctr.homestaybooking.repository.ReviewRepository
 import com.ctr.homestaybooking.repository.UserRepository
+import com.ctr.homestaybooking.shared.BASE_URL_RECOMMEND
+import com.ctr.homestaybooking.shared.log
 import com.ctr.homestaybooking.shared.toNullable
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.util.MultiValueMap
+import org.springframework.web.client.RestTemplate
+
 
 /**
  * Created by at-trinhnguyen2 on 2020/11/03
@@ -35,6 +44,17 @@ class ReviewService(private val reviewRepository: ReviewRepository,
                 throw  ReviewIsDuplicateException(it.id)
             }
         }
+        val uri = "$BASE_URL_RECOMMEND/add_review/"
+        val headers = HttpHeaders()
+        headers.contentType = MediaType.APPLICATION_FORM_URLENCODED
+        val map: MultiValueMap<String, String> = LinkedMultiValueMap<String, String>()
+        map.add("place_id", reviewEntity.placeEntity?.id.toString())
+        map.add("user_id", reviewEntity.userEntity.id.toString())
+        map.add("rating", reviewEntity.rating.toString())
+        val request = HttpEntity(map, headers)
+        val restTemplate = RestTemplate()
+        val response = restTemplate.postForObject(uri, request, String::class.java)
+        response.apply { log.info { this } }
 
         return reviewRepository.save(reviewEntity).apply {
             reviewEntity.bookingEntity?.id?.let { id ->
